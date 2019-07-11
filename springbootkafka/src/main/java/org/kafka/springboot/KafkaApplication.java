a87e99d3f1347216e3c6b2a6ce43e2bd3c3c86ac
mengHeJiuQian <1197563121@qq.com>
2019-07-11 10:08:03 +0800

    提交以前还没整合好springboot和kafka的代码，

--- springbootkafka/src/main/java/org/kafka/springboot/KafkaApplication.java
+++ springbootkafka/src/main/java/org/kafka/springboot/KafkaApplication.java
@@ -8,12 +8,9 @@ import org.springframework.context.ConfigurableApplicationContext;
 /**
  * describe:
  *
- *
  * https://blog.csdn.net/a15835774652/article/details/81363164
  * kafka通过节点名去查找服务器，所以要配置服务端的 advertised.listeners=PLAINTEXT://izuf660km9nbcwquzfy6yiz:9092
  * 本地也要配置服务端的ip和机器名映射
- *
- *
  *
  * @author 梦合九千
  * @date 2019/5/17 14:21
