7158d8a9438fcb7f988d4ac3487eab924ee47955
mengHeJiuQian <1197563121@qq.com>
2019-07-19 14:00:26 +0800

    查阅hashmap底层resize()实现方式

--- test_anta/src/main/java/map/HashMapTest.java
+++ test_anta/src/main/java/map/HashMapTest.java
@@ -16,42 +16,58 @@ public class HashMapTest {
 
     public static final int TEST_NUM = 1_000_000; // 测试量
 
-//    public static void main(String[] args) {
-////        HashMapTest();
+    public static void main(String[] args) {
+//        hashMapTest();
+//        testConcurrentHashMap();
+
 //        // testContainsKey();
-////        testConcurrentHashMap();
 //        System.out.println(0x7fffffff);
 //        System.out.println(1 << 30);
 //
 //        testCHashMap_tableSizeFor();
-//
-//    }
+
+        HashMap map = new HashMap();
+        map.put("a", 1);
+        map.put("b", 2);
+        map.put("b", 3);
+        map.put("b", 4);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("b", 1);
+
+    }
 
     public static void testCHashMap_tableSizeFor() {
-        int initialCapacity = 10;
+        /*int initialCapacity = 10;
         int sc = initialCapacity + (initialCapacity >>> 1) + 1;
         System.out.println(sc);
         int i = tableSizeFor(sc);
-        System.out.println(i);
+        System.out.println(i);*/
     }
 
-    private static int tableSizeFor(int c) {
-        int n = c - 1;
+    /**
+     * 测试hashMap怎么做到设置数组大小为2的整数次幂的
+     */
+    @Test
+    public void tableSizeFor() {
+        int MAXIMUM_CAPACITY = 1 << 30;
+        int c = 1;
+        int n = c - 1; // 减一是为了对于已经是二次幂的容量大小操作依然是该二次幂
         n |= n >>> 1;
         n |= n >>> 2;
         n |= n >>> 4;
         n |= n >>> 8;
         n |= n >>> 16;
-        return n + 1;
+        n = (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+        System.out.println(n);
     }
 
-    public static void HashMapTest() {
+    private  static void hashMapTest() {
         HashMap map = new HashMap();
-//        map.put(new Object(), 1);
-//        map.put(new Object(), 2);
-//        System.out.println(map.size());
-
-
 
         long t1 = System.currentTimeMillis();
         for (int i = 0; i < TEST_NUM; i++) {
@@ -68,7 +84,6 @@ public class HashMapTest {
         if (null == null) {
             System.out.println("sadf");
         }
-        System.out.println(null == null);
         System.out.println("null == null ? " + null == null);
         System.out.println("真null假null都是null：" + map.get(1));
         System.out.println("真null假null都是null：" + map.get(2));
