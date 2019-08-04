4000bbbfd9910bbf3057ca7869adda0374afde89
mengHeJiuQian <1197563121@qq.com>
2019-08-04 21:52:40 +0800

    jdk10 var关键字

---
+++ test-jdk11/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var i = "yangliu";
+        System.out.println(i);
+        var j = 1;
+        var list = new ArrayList<String>();
+        System.out.println("hello world");
+
+        var linked = new LinkedList();
+
+    }
+}
