a817ab540051eab8246d891c9ffcc0aa47fc7cd7
mengHeJiuQian <1197563121@qq.com>
2019-08-05 12:30:47 +0800

    +=操作符会自动进行类型转换，与左边的变量类型一致

--- test-jdk11/src/Main.java
+++ test-jdk11/src/Main.java
@@ -12,5 +12,27 @@ public class Main {
 
         var linked = new LinkedList();
 
+        int[] ints = {1,2,3,4,5};
+        System.out.println(squareOfSum(ints));
+
+        IntAddDouble();
+    }
+
+    public static int squareOfSum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            double pow = Math.pow(arr[i], 2);//java中的次方表示放式：Math.pow(2, 3); 2的3次方
+            sum += pow;
+
+        }
+        return sum;
+    }
+
+    public static void IntAddDouble() {
+        int i = 10;
+        double d = 12.2;
+        int r = 2;
+        r += i * d;
+        System.out.println(r);
     }
 }
