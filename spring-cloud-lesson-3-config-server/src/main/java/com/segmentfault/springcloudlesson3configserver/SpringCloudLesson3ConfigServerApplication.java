85163c58e2fa93e0498cbcb8542d03de554ca72c
mengHeJiuQian <1197563121@qq.com>
2019-09-17 00:38:31 +0800

    spring cloud lesson 3 第35min 学习使用config server。

---
+++ spring-cloud-lesson-3-config-server/src/main/java/com/segmentfault/springcloudlesson3configserver/SpringCloudLesson3ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.segmentfault.springcloudlesson3configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudLesson3ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudLesson3ConfigServerApplication.class, args);
+		// System.out.println(System.getProperty("user.dir"));
+	}
+
+}
