d03423e767d87c93236f7c915db0d6d29342e2eb
mengHeJiuQian <1197563121@qq.com>
2019-09-26 00:09:00 +0800

    追踪解决方案及zipkin

---
+++ sc-f-chapter09/service-zipkin/src/main/java/com/forezp/ServiceHiApplication.java
@@ -0,0 +1,60 @@
+package com.forezp;
+
+import brave.sampler.Sampler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 创建人：yang.liu
+ * 创建时间：2019/9/25 23:49
+ * 版本：1.0
+ * 内容描述：
+ */
+@SpringBootApplication
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
+
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping("/hi")
+    public String callHome() {
+        LOG.log(Level.INFO, "calling trace service-hi  ");
+        return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+    }
+
+    @RequestMapping("/info")
+    public String info() {
+        LOG.log(Level.INFO, "calling trace service-hi ");
+
+        return "i'm service-hi";
+
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+
+}
