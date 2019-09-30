fb1177b21b449f102ad0b25537a6ca7a4b58e608
mengHeJiuQian <1197563121@qq.com>
2019-09-30 15:14:08 +0800

    整理test_anta project

--- test_anta/src/main/java/book1/chapter03/replace_method/ReplacerTest.java
+++ test_anta/src/main/java/book1/chapter03/replace_method/ReplacerTest.java
@@ -2,11 +2,12 @@ package book1.chapter03.replace_method;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class ReplacerTest {
     public static void main(String[] args) {
-        String configLocation = "classpath:book1/chapter03/replace_method/applicationContext.xml";
-        ApplicationContext ac = new ClassPathXmlApplicationContext(configLocation);
+        String configLocation = "src/main/java/book1/chapter03/replace_method/applicationContext.xml";
+        ApplicationContext ac = new FileSystemXmlApplicationContext(configLocation);
         TestChangeMethod test = (TestChangeMethod) ac.getBean("testChangeMethod");
         test.changeMe();
     }
