61ca909e947e87047bdfb5be2f87667b4a2eb783
mengHeJiuQian <1197563121@qq.com>
2019-09-30 18:20:19 +0800

    测试Gson和FastJson，感觉差不多。

--- test_anta/src/main/java/json/JsonTest.java
+++ test_anta/src/main/java/json/JsonTest.java
@@ -1,10 +1,15 @@
 package json;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.junit.jupiter.api.Test;
 import org.redisson.misc.Hash;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,6 +51,45 @@ public class JsonTest {
         map.put("userInfos", list);
 
         System.out.println(JSON.toJSONString(map));
+    }
+
+    @Test
+    public void testGson() {
+        List<Map<String, Integer>> list = new ArrayList<>();
+        for (int i = 1; i <= 1_000_000; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            map.put(String.valueOf(i), i);
+            list.add(map);
+        }
+        Instant t1 = Instant.now();
+        Gson gson = new Gson();
+        String json = gson.toJson(list);
+        Instant t2 = Instant.now();
+        System.out.println(Duration.between(t1,t2).toMillis());
+
+        Instant t3 = Instant.now();
+        List list1 = gson.fromJson(json, List.class);
+        Instant t4 = Instant.now();
+        System.out.println(Duration.between(t3,t4).toMillis());
+    }
+
+    @Test
+    public void testFastJson() {
+        List<Map<String, Integer>> list = new ArrayList<>();
+        for (int i = 1; i <= 1_000_000; i++) {
+            Map<String, Integer> map = new HashMap<>();
+            map.put(String.valueOf(i), i);
+            list.add(map);
+        }
+        Instant t1 = Instant.now();
+        String json = JSONObject.toJSONString(list);
+        Instant t2 = Instant.now();
+        System.out.println(Duration.between(t1,t2).toMillis());
+
+        Instant t3 = Instant.now();
+        List list1 = JSONObject.parseObject(json, List.class);
+        Instant t4 = Instant.now();
+        System.out.println(Duration.between(t3,t4).toMillis());
     }
 
 }
