23aabbba6dc906f602d738e14f1f78a7cbbc7fae
mengHeJiuQian <1197563121@qq.com>
2019-10-08 15:36:56 +0800

    整理springboot-demo project

---
+++ springboot-demo/src/main/java/com/example/SpringBootDemoApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Author: sheldon
+ * @Create Date: 2019/9/30 15:38
+ * @Update Date: 2019/9/30 15:38
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+}
