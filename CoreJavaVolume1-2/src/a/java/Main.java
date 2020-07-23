ceb16ee9c83974d8893a583de86174275e8baf18
mengHeJiuQian <1197563121@qq.com>
2020-07-23 21:58:42 +0800

    删除无用代码

--- CoreJavaVolume1-2/src/a/java/Main.java
+++
@@ -1,9 +0,0 @@
-package a.java;
-
-// 一级包名不能叫“java”，会报“java.lang.SecurityException: Prohibited package name: java.mypackage”
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- dubbo-one/api/src/main/java/yang/liu/dubbo/one/api/service/DubboRecordService.java
+++
@@ -1,9 +0,0 @@
-package yang.liu.dubbo.one.api.service;
-
-import yang.liu.dubbo.one.api.request.PushOrderDto;
-import yang.liu.dubbo.one.api.response.BaseResponse;
-
-public interface DubboRecordService {
-
-    public BaseResponse pushOrder(PushOrderDto dto);
-}
