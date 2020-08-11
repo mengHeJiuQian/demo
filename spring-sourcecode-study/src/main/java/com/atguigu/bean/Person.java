32a5df4b4d0811e1a34950154e216a9535ea8ef2
mengHeJiuQian <1197563121@qq.com>
2020-08-11 23:00:43 +0800

    @Value注解使用

--- spring-sourcecode-study/src/main/java/com/atguigu/bean/Person.java
+++ spring-sourcecode-study/src/main/java/com/atguigu/bean/Person.java
@@ -1,12 +1,18 @@
 package com.atguigu.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+
 /**
  * @author yang.liu
  */
 public class Person {
 
+    @Value("张三")
     private String name;
+    @Value("#{20 + 5}")
     private Integer age;
+    @Value("${person.nickName}")
+    private String nickName;
 
     public Person() { }
 
@@ -20,6 +26,7 @@ public class Person {
         return "Person{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", nickName='" + nickName + '\'' +
                 '}';
     }
 }
