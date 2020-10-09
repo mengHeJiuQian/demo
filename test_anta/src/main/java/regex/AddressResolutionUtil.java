cde39972f7f847600f3b935b902af2113df278c8
yang.liu <1197563121@qq.com>
2020-10-09 19:10:29 +0800

    正则获取乡镇

--- test_anta/src/main/java/regex/AddressResolutionUtil.java
+++ test_anta/src/main/java/regex/AddressResolutionUtil.java
@@ -1,9 +1,7 @@
 package regex;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,30 +18,11 @@ public class AddressResolutionUtil {
      * @param address
      * @return
      */
-    public static List<Map<String,String>> addressResolution(String address){
-//        String regex="(?<province>[^省]+自治区|.*?省|.*?行政区|.*?市)(?<city>[^市]+自治州|.*?地区|.*?行政单位|.+盟|市辖区|.*?市|.*?县)(?<county>[^县]+县|.+区|.+市|.+旗|.+海域|.+岛)?(?<town>[^区]+区|.+镇)?(?<village>.*)";
-//        String regex="(?<province>[^省]+省|.+自治区)(?<city>[^自治州]+自治州|[^市]+市|[^盟]+盟|[^地区]+地区|.+区划)(?<county>[^市]+市|[^县]+县|[^旗]+旗|.+区)?(?<town>[^区]+区|.+镇)?(?<village>.*)";
-//        String regex="(?<province>[^省]+自治区|.*?省|.*?行政区|.*?市)(?<city>[^市]+自治州|.*?地区|.*?行政单位|.+盟|市辖区|.*?市|.*?县)(?<county>[^县]+县|.+区|.+市|.+旗|.+海域|.+岛)";
-//        String regex="((?<province>[^省]+省|.+自治区)|上海|北京|天津|重庆)(?<city>[^市]+市|.+自治州)(?<county>[^县]+县|.+区|.+镇|.+局)?(?<town>[^区]+区|.+镇)?(?<village>.*)";
-        String regex = "(?<province>[^省]+省|.+自治区)(?<city>[^自治州]+自治州|[^市]+市|[^盟]+盟|[^地区]+地区|.+区划)(?<county>[^市]+市|[^县]+县|[^旗]+旗|.+区)?(?<town>[^区]+区|.+镇)?(?<village>.*)";
-        Matcher m= Pattern.compile(regex).matcher(address);
-        String province=null,city=null,county=null,town=null,village=null;
-        List<Map<String,String>> table=new ArrayList<Map<String,String>>();
-        Map<String,String> row=null;
-        while(m.find()){
-            row=new LinkedHashMap<String,String>();
-            province=m.group("province");
-            row.put("province", province==null?"":province.trim());
-            city=m.group("city");
-            row.put("city", city==null?"":city.trim());
-            county=m.group("county");
-            row.put("county", county==null?"":county.trim());
-//            town=m.group("town");
-//            row.put("town", town==null?"":town.trim());
-//            village=m.group("village");
-//            row.put("village", village==null?"":village.trim());
-            table.add(row);
-        }
+    public static List<String> addressResolution(String address){
+        String regex = "";
+        Matcher m = Pattern.compile(regex).matcher(address);
+        String town = null, village = null;
+        List<String> table = new ArrayList<>();
         return table;
     }
 
