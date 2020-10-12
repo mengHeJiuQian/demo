b7174967332433ae4500d6be67bae27438c94f67
wangye <1197563121@qq.com>
2020-10-12 22:53:04 +0800

    optional使用

--- Java8InAction/src/chapter10_Optional/optional/TestOptional.java
+++ Java8InAction/src/chapter10_Optional/optional/TestOptional.java
@@ -1,4 +1,4 @@
-package optional;
+package chapter10_Optional.optional;
 
 import java.util.Optional;
 
@@ -9,8 +9,7 @@ public class TestOptional {
         Optional<Object> optionalObj1 = Optional.empty();
 
         // 2.依据一个非空值创建Optional
-        Object obj2 = null;
-        // Optional<Object> optionalObj2 = Optional.of(obj2); // 会报 NullPointerException
+        Object obj2 = null;        // Optional<Object> optionalObj2 = Optional.of(obj2); // 会报 NullPointerException
 
         // 3.可接受null的Optional
         Object obj3 = null;
