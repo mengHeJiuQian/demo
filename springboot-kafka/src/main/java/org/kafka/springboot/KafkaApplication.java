64c92190833876a275243298cc21464051959251
yang.liu <1197563121@qq.com>
2020-11-15 23:17:46 +0800

    commit

--- springboot-kafka/src/main/java/org/kafka/springboot/KafkaApplication.java
+++ springboot-kafka/src/main/java/org/kafka/springboot/KafkaApplication.java
@@ -23,18 +23,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 public class KafkaApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(KafkaApplication.class, args);
-        /*KafkaSender sender = context.getBean(KafkaSender.class);
-        for (int i = 0; i < 3; i++) {
-            //调用消息发送类中的消息发送方法
-            sender.send();
-
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }*/
+        SpringApplication.run(KafkaApplication.class, args);
     }
 
 }
